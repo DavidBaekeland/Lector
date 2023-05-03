cb44153dafb799408bcb28bf28b2eabc2bc5f524
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-05-03 10:12:26 +0200

    Update: logo, card, input, labels, error in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,42 +6,73 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
+        <div class="input-div">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-text-input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')"/>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="input-div">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
+
+        <div class="flex">
+            <!-- Remember Me -->
+            <div class="remember-div">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <!-- Reset password -->
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="password-reset" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
+
+
+        <x-primary-button>
+            {{ __('LOGIN') }}
+        </x-primary-button>
     </form>
+
+    <style>
+        .text-input[type="email"] {
+            background: url({{ asset('storage/icons/email.svg') }}) no-repeat;
+            background-size: 20px;
+            background-position: 10px center;
+        }
+
+        .text-input[type="password"] {
+            background: url({{ asset('storage/icons/password.svg') }}) no-repeat;
+            background-size: 20px;
+            background-position: 10px center;
+        }
+
+        .text-input[type="email"]:focus {
+            background: url({{ asset('storage/icons/selected/email.svg') }}) no-repeat;
+            background-size: 20px;
+            background-position: 10px center;
+        }
+
+        .text-input[type="password"]:focus {
+            background: url({{ asset('storage/icons/selected/password.svg') }}) no-repeat;
+            background-size: 20px;
+            background-position: 10px center;
+        }
+    </style>
 </x-guest-layout>
