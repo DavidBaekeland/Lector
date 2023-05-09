58d11950433d7e805ece9d231a6230d41ed67cb0
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-05-09 14:38:22 +0200

    Add: admin can filter user table

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,10 +9,10 @@
             @csrf
 
             <div class="user-links">
-                <x-text-input id="search-users" type="email" name="search-users" />
+                <x-text-input id="search-users" name="search-users" autocomplete="off" />
 
                 <div class="change-user-links">
-                    <a href="{{ route('users.create') }}" class="link">
+                    <a class="link" id="register-modal-link">
                         <x-icon>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
@@ -40,10 +40,10 @@
                         </tr>
                     </thead>
 
-                    <tbody>
+                    <tbody id="user-body">
                         @foreach($users as $user)
                             <tr>
-                                <td><input type="checkbox" name="users[]" id="" class="checkbox" value="{{ $user->id }}"></td>
+                                <td><input type="checkbox" name="users[]" class="checkbox" value="{{ $user->id }}"></td>
                                 <td>{{$user->first_name}}</td>
                                 <td>{{$user->last_name}}</td>
                                 <td>{{$user->email}}</td>
@@ -66,6 +66,47 @@
             </div>
         </form>
     </x-card-large>
+
+
+    <script>
+        let userBodyTable = document.getElementById("user-body");
+
+        let searchUsersInput = document.getElementById('search-users');
+
+        let usersPHP = {!! json_encode($users->toArray()) !!};
+
+        let usersJS = usersPHP;
+
+        searchUsersInput.addEventListener("keyup", (e) => {
+            userBodyTable.innerHTML = "";
+
+            if(e.target.value == 0) {
+                usersJS = usersPHP;
+            } else {
+                usersJS = usersJS.filter(user => String(user.first_name).startsWith(e.target.value))
+            }
+
+
+            usersJS.forEach(user => {
+                console.log()
+                let htmlString = `
+                <tr>
+                    <td><input type="checkbox" name="users[]" class="checkbox" value="${ user.id }"></td>
+                    <td>${user.first_name}</td>
+                    <td>${user.last_name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.role.name}</td>
+                    <td>${((user.course != null) ? user.course.name : 'N/A')}</td>
+                    <td>${((user.year != null) ? user.year : 'N/A')}</td>
+                </tr>
+                `
+
+                userBodyTable.insertAdjacentHTML("beforeend", htmlString)
+            })
+        })
+
+
+    </script>
 
     <style>
         #search-users {
