a162155cfd9d18089cab3d78ad048cea03369712
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-05-12 10:27:38 +0200

    Update: css chat form

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -20,9 +20,17 @@
             <x-input-hidden id="chat_id" value="{{auth()->user()->chats->first()->id}}" />
 
             <p id="typing"></p>
-            <x-text-input type="text" name="messageInput" id="messageInput" autocomplete="off" />
 
-            <button type="submit" id="submit-chat">Submit</button>
+            <div id="chat-input-div">
+                <x-text-input type="text" name="messageInput" id="messageInput" autocomplete="off" />
+
+                <button type="submit" id="submit-chat">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                    </svg>
+                </button>
+            </div>
+
         </form>
     </x-card-large>
 
