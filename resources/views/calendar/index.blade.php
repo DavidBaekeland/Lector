525e0eed23f11429ecbb4edcf17f2263ffaad1ac
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-05-23 12:19:21 +0200

    Add: styling to calendar

---
+++ resources/views/calendar/index.blade.php
@@ -0,0 +1,105 @@
+<x-app-layout>
+    <x-card-large>
+        <div class="dates">
+            <h1> </h1>
+            <h1>Ma - 22/5</h1>
+            <h1>Di - 23/5</h1>
+            <h1>Wo - 24/5</h1>
+            <h1>Do - 25/5</h1>
+            <h1>Vr - 26/5</h1>
+        </div>
+        <div id="calendar">
+            <div class="time">
+                <p class="calendar-time">09:00</p>
+                <p class="calendar-time">10:00</p>
+                <p class="calendar-time">11:00</p>
+                <p class="calendar-time">12:00</p>
+                <p class="calendar-time">13:00</p>
+                <p class="calendar-time">14:00</p>
+                <p class="calendar-time">15:00</p>
+                <p class="calendar-time">16:00</p>
+                <p class="calendar-time">17:00</p>
+                <p class="calendar-time">18:00</p>
+                <p class="calendar-time">19:00</p>
+                <p class="calendar-time">20:00</p>
+            </div>
+            <div class="days">
+                <div class="day">
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="active-course">
+                        <span class="info-active">
+                            <h2>Design V</h2>
+                            <span>B1.022</span>
+                        </span>
+                    </div>
+                    <div class="calendar-item"></div>
+                    <div class="active-course-2">
+                        <span class="info-active">
+                            <h2>Expert Lab</h2>
+                            <span>B1.022</span>
+                        </span>
+                    </div>
+                    <div class="calendar-item"></div>
+                </div>
+                <div class="day">
+                    <div class="active-course-4">
+                        <span class="info-active">
+                            <h2>Expert Lab</h2>
+                            <span>B1.022</span>
+                        </span>
+                    </div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                </div>
+                <div class="day">
+                    <div class="active-course-5">
+                        <span class="info-active">
+                            <h2>Expert Lab</h2>
+                            <span>B1.022</span>
+                        </span>
+                    </div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                </div>
+
+                <div class="day">
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+                    <div class="calendar-item"></div>
+
+                    <div class="active-course-8">
+                        <span class="info-active">
+                            <h2>Expert Lab</h2>
+                            <span>B1.022</span>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="active-course-12">9</div>
+                </div>
+            </div>
+
+        </div>
+    </x-card-large>
+
+    @vite(['resources/css/calendar.css', 'resources/js/calendar.js'])
+</x-app-layout>
