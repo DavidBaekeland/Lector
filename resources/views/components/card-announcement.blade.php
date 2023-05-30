2fd1ec01d2f0789545d6fdd5a674b7c90137f9af
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-05-30 15:53:59 +0200

    Update: replace position announcement to component

--- resources/views/components/card-announcement.blade.php
+++ resources/views/components/card-announcement.blade.php
@@ -1,3 +1,20 @@
-<div {{$attributes->merge(['class' => "card card-large card-announcement" ])}} >
-    {{$slot}}
+@props([
+    "announcement"
+])
+
+<div {{$attributes->merge(['class' => "card card-large card-announcement" ])}} x-data="{ open: false }" x-on:click="open = ! open">
+    <div x-bind:class="open ? 'announcement-title-active' : 'announcement-title'">
+        <h3 >{{ $announcement->title }}</h3>
+        @if($announcement->created_at->format("d-m-Y") == now()->format("d-m-Y"))
+            {{$announcement->created_at->format("H:m")}}
+        @else
+            {{$announcement->created_at->format("d-m-Y")}}
+        @endif
+    </div>
+    <span x-show="open">
+        <hr>
+        <p class="announcement">
+            {!! $announcement->announcement !!}
+        </p>
+    </span>
 </div>
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -13,22 +13,7 @@
 
     <x-card-large id="subject-card">
         @foreach($announcements as $announcement)
-            <x-card-announcement x-data="{ open: false }" x-on:click="open = ! open">
-                <div x-bind:class="open ? 'announcement-title-active' : 'announcement-title'">
-                    <h3 >{{ $announcement->title }}</h3>
-                    @if($announcement->created_at->format("d-m-Y") == now()->format("d-m-Y"))
-                        {{$announcement->created_at->format("H:m")}}
-                    @else
-                        {{$announcement->created_at->format("d-m-Y")}}
-                    @endif
-                </div>
-                <span x-show="open">
-                    <hr>
-                    <p class="announcement">
-                        {!! $announcement->announcement !!}
-                    </p>
-                </span>
-            </x-card-announcement>
+            <x-card-announcement :announcement="$announcement" />
         @endforeach
     </x-card-large>
 </x-app-layout>
