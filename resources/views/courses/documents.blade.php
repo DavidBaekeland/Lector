e9bb89685c269cd2f3a71f18701189e0f9fd6f85
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-06-11 23:04:57 +0200

    Delete: back buttons on desktop version

--- resources/views/courses/documents.blade.php
+++ resources/views/courses/documents.blade.php
@@ -26,11 +26,7 @@
                         </svg>
                     </button>
                 @else
-                    <a id="course-back" href="{{ route('courses.index') }}" class="videoLink backLinkCourse">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                        </svg>
-                    </a>
+                    <span></span>
                 @endcan
 
                 <h3 id="titleSubject">{{ $selectedSubject->name }}</h3>
